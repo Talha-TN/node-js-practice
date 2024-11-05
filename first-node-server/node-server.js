4e09c4e9ceb2adfcac8b2426e3c8c803a497f4ed
Talha-TN <nawazt964@gmail.com>
2024-11-05 12:11:34 -0800

    learning request and response in node js

--- first-node-server/node-server.js
+++ first-node-server/node-server.js
@@ -1,12 +1,19 @@
-
-const http = require("http")
+const http = require("http");
 // const requestListener=(req,res)=>{ // using normal function
 //     console.log(req)
 // }
 
 // http.createServer(requestListener) // normal function passing here as argumant
 
-const server = http.createServer((req,res)=>{  // using arrow function or anonymous function here both works as same 
-    console.log(req)
-})
-server.listen(3000)
+const server = http.createServer((req, res) => {
+  // using arrow function or anonymous function here both works as same
+  // console.log(req);
+  console.log(req.url,req.method,req.headers)
+  // process.exit() // stops the server
+});
+// server.listen(3000)
+// another way
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`server running at https://localhost:${PORT}`);
+});
