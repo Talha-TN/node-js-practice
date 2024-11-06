621c2e08efa9e99b688a239fe741f2168dc206d7
Talha-TN <nawazt964@gmail.com>
2024-11-06 12:22:46 -0800

    does practice

--- first-node-server/node-server.js
+++ first-node-server/node-server.js
@@ -1,19 +1,87 @@
-const http = require("http");
-// const requestListener=(req,res)=>{ // using normal function
-//     console.log(req)
-// }
+// const http = require("http");
+// // const requestListener=(req,res)=>{ // using normal function
+// //     console.log(req)
+// // }
 
-// http.createServer(requestListener) // normal function passing here as argumant
+// // http.createServer(requestListener) // normal function passing here as argumant
+// const myName = "Talha Nawaz"
+// const prof = "software engineer"
+// const server = http.createServer((req, res) => {
+//   // using arrow function or anonymous function here both works as same
+//   // console.log(req);
+//   // console.log(req.url,req.method,req.headers)
+//   if(req.url==="/"){
+//     res.setHeader("content-Type" ,"text/html")
+//     res.write("<html>")
+//     res.write("<head> <title> header page </title></head>")
+//     res.write(`<body><h1>This is home page</h1></body>`)
+//     res.write("</html>")
+//     return res.end()
+
+//   }else if (req.url==='/products'){
+//     res.setHeader("content-Type" ,"text/html")
+//     res.write("<html>")
+//     res.write("<head> <title> products page </title></head>")
+//     res.write(`<body><h1> This is products page</h1></body>`)
+//     res.write("</html>")
+//     return res.end()
+//   }else{
+//     res.setHeader("content-Type" ,"text/html")
+//     res.write("<html>")
+//     res.write("<head> <title> user page </title></head>")
+//     res.write(`<body><h1> like share and subscribe  , my name is ${myName} ${prof} </h1></body>`)
+//     res.write("</html>")
+//     res.end()
+//   }
+
+//   // process.exit() // stops the server
+// });
+// // server.listen(3000)
+// // another way
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//   console.log(`server running at https://localhost:${PORT}`);
+// });
+
+////////////////////////////////////////////////////////////////////////////
+const http = require("http");
+
+const myName = "Talha Nawaz";
+const prof = "software engineer";
 
 const server = http.createServer((req, res) => {
-  // using arrow function or anonymous function here both works as same
-  // console.log(req);
-  console.log(req.url,req.method,req.headers)
-  // process.exit() // stops the server
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Header Page</title></head>");
+    res.write(`<body><h1>This is the home page</h1></body>`);
+    res.write(`<body><h1>enter your details</h1></body>`);
+    res.write("<form>");
+    res.write('<input type="text" name="username" placeholder="enter user name">');
+
+    res.write("</form>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Header Page</title></head>");
+    res.write(`<body><h1>This is the products page</h1></body>`);
+    res.write("</html>");
+    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");         
+    res.write("<html>");
+    res.write("<head><title>Header Page</title></head>");
+    res.write(
+      `<body><h1>Like, share, and subscribe. My name is ${myName}, ${prof}.</h1></body>`
+    );
+    res.write("</html>");
+    res.end();
+  }
 });
-// server.listen(3000)
-// another way
+
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`server running at https://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
---
+++ first-node-server/practice.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.write(
+    `
+<html>
+<head>
+   
+</head>
+<body>
+    <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">contact</a></li>
+        <li><a href="/services">services</a></li>
+        
+    </ul>
+</body>
+</html>
+        `
+  );
+  if(req.url.toLowerCase()==="/home"){
+     
+    res.write(`<h1>This is home page</h1>`);
+    return res.end()
+  }
+  else if(req.url.toLowerCase()==="/About"){
+   
+    res.write(`<h1>This is home page</h1>`);
+    return res.end()
+  }
+  else if(req.url.toLowerCase()==="/contact"){
+  
+    res.write(`<h1>This is home page</h1>`);
+    return res.end()
+  }
+  else if(req.url.toLowerCase()==="/services"){
+    res.write(`<h1>This is home page</h1>`);
+    return res.end()
+  }
+
+  res.end();
+});
+server.listen(3000, () => {
+  console.log(`the server has been started at http://localhost:3000`);
+});
